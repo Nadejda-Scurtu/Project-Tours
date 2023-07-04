eac9570637a419181c3b9b84382661caecea3d7a
Nadejda-Scurtu <nadejda.scurtu@isa.utm.md>
2023-07-04 17:49:01 +0300

    header first

--- project-tours/src/main.js
+++ project-tours/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import router from './router.js'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
