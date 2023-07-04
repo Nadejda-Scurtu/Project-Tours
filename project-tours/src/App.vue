16609d718c659470f1f17ce10d7e9f48d3ec7940
Nadejda-Scurtu <nadejda.scurtu@isa.utm.md>
2023-07-04 10:42:20 +0300

    init

---
+++ project-tours/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <header-navbar/>
+  </div>
+</template>
+
+<script>
+import HeaderNavbar from './components/project_tours/header/HeaderNavbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderNavbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
