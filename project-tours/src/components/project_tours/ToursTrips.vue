ed16c2877f6aecd7d8ac7280ad2a83a0950cc5ba
Nadejda-Scurtu <nadejda.scurtu@isa.utm.md>
2023-07-10 16:24:06 +0300

    nav-slider-footer

---
+++ project-tours/src/components/project_tours/ToursTrips.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <header-navbar />
+  <slider-tours/>
+  <footer-tours/>
+  </div>
+ </template>
+ 
+ <script>
+ import HeaderNavbar from "./header/HeaderNavbar.vue";
+ import SliderTours from "./slider/SliderTours.vue";
+ import FooterTours from "./footer/FooterTours.vue"
+ 
+ export default {
+   components: {
+     HeaderNavbar,
+     SliderTours,
+     FooterTours
+   },
+ };
+ </script>
